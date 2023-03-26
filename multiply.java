1436569c57bbc348831775204f84073170b95472
rashi1405 <rashijaiswal1405@gmail.com>
2023-03-26 14:39:26 +0530

    initial commit

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class multiply {
+
+  public static void main(String[] args) {
+    
+    int f = 10;
+    int s = 20;
+
+    System.out.println(f * s);
+  }
+}
---
+++ sum.java
@@ -0,0 +1,10 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    int f = 10;
+    int s = 20;
+
+    System.out.println(f + s);
+  }
+}
